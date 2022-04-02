d464bf2ab2bd472323b52fcb6b54e41cfa44c0be
yangmx <506357844@qq.com>
2022-04-02 13:33:14 +0800

    测试项目基本配置

---
+++ jimureport/src/main/java/com/Application.java
@@ -0,0 +1,18 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * @Date 2022/4/1 13:45
+ * @Created by yangmx
+ */
+@SpringBootApplication
+public class Application {
+        public static void main(String[] args) {
+                SpringApplication.run(Application.class, args);
+            }
+
+
+}
