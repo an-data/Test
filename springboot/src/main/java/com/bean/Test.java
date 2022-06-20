74ba8b792963d09bd1bad2e456eef3f5ae2ca02b
yangmx <506357844@qq.com>
2022-06-20 08:36:57 +0800

    测试项目基本配置

---
+++ springboot/src/main/java/com/bean/Test.java
@@ -0,0 +1,15 @@
+package com.bean;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.util.Date;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+
+    }
+
+}
